ebe3986eb3448e8e1e2a8c0bcf7ec8dbcfd9dd46
Dhirajzen <117260766+Dhirajzen@users.noreply.github.com>
2026-02-22 03:59:52 +0000

    Refactor spi_coverage class: move build_phase function and update initialization

--- SPI_UVM_Verification/coverage.sv
+++ SPI_UVM_Verification/coverage.sv
@@ -5,12 +5,14 @@ class spi_coverage extends uvm_subscriber;
   uvm_analysis_imp#(spi_transaction, spi_coverage) cov_imp;
   spi_transaction tr;
 
-  
-  function new(string name, uvm_component parent);
-    super.new(name, parent);
-   check = new();
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    cov_imp = new("cov_imp", this);
+     
+    `uvm_info("COV", $sformatf("COVER build Passed"), UVM_MEDIUM);
   endfunction
   
+
 
     covergroup check;
         option.per_instance = 1;
@@ -32,13 +34,12 @@ class spi_coverage extends uvm_subscriber;
         cross tr.wr, tr.addr, tr.din;
     endgroup
   
-
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-    cov_imp = new("cov_imp", this);
-     
-    `uvm_info("TEST", $sformatf("COVER build Passed"), UVM_MEDIUM);
+    function new(string name = "spi_coverage", uvm_component parent = null);
+    super.new(name, parent);
+   check = new();
   endfunction
+  
+
   
 
 
