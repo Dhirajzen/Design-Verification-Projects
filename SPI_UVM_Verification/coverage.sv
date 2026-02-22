0dc6dd13cf4170039d0a685b54fd8483f2a66141
Dhirajzen <117260766+Dhirajzen@users.noreply.github.com>
2026-02-22 05:05:42 +0000

    Refactor coverage: simplify coverpoint for tr.wr by removing explicit bins

--- SPI_UVM_Verification/coverage.sv
+++ SPI_UVM_Verification/coverage.sv
@@ -18,10 +18,8 @@ class spi_coverage extends uvm_subscriber#(spi_transaction);
         option.per_instance = 1;
         option.auto_bin_max = 8;
 
-        coverpoint tr.wr {
-            bins wr_0 = {0};
-            bins wr_1 = {1};
-        }
+        coverpoint tr.wr;
+        
         coverpoint tr.addr {
             option.at_least = 8;
         }
